b0151fec38860f96468140b55bac2b904e0f0f60
ssj4dofuturo <felip3.fl@gmail.com>
2020-09-07 14:43:09 -0300

    80. Adicionando Serviços a um Módulo Compartilhado

--- meat-app-starter-master/src/app/shared/shared.module.ts
+++ meat-app-starter-master/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModel, ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -14,4 +17,11 @@ import { CommonModule } from "@angular/common";
               FormsModule, ReactiveFormsModule]
 })
 
-export class SharedModule {}
+export class SharedModule {
+    static forRoot(): ModuleWithProviders{
+        return {
+            ngModule: SharedModule,
+            providers:[ShoppingCartService, RestaurantsService, OrderService]
+        }
+    }
+}
