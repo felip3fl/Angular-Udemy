49392e1664d52efdc4189e1f7b8a8e937fd80a15
ssj4dofuturo <felip3.fl@gmail.com>
2021-10-10 20:37:06 -0300

    Instalacao bibliotecas

--- source/fl-meat/src/app/app.component.ts
+++ source/fl-meat/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'fl-meat';
+export class AppComponent implements OnInit {
+  content = 'Oioi  do Meat App!'
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
